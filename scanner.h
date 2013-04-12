fd1d404014d87011df33f977243e2e11930c193c
Pascal Boeschoten <passiepascal@gmail.com>
2013-04-12 16:27:31 +0300

    Scanner improvements.

--- scanner.h
+++ scanner.h
@@ -9,22 +9,33 @@
 
 #define TURRET_ROTATION_AMOUNT 20
 
+int scanner_currentRotation = 0;
+
+void turnTurret(int deg) {
+    RotateMotor(MOTOR_TURRET_OUT, MOTOR_TURRET_PWR, deg);
+    scanner_currentRotation += deg;
+}
+
+void centerTurret() {
+    RotateMotor(MOTOR_TURRET_OUT, MOTOR_TURRET_PWR,
+     -scanner_currentRotation);
+    scanner_currentRotation = 0;
+}
+
 // Procedure for making a full scan
-void ultrasonic_scan() {
-    int currentRotation = 0;
+// Returns an angle where the free direction is, or 0 if there was no
+// direction found.
+int ultrasonic_scan() {
     int sensorValue = 0;
     SetSensorUltrasonic(ULTRASONIC_IN);
     
     // Turn turret completely to the left
-    RotateMotor(MOTOR_TURRET_OUT, MOTOR_TURRET_PWR, -180);
-    currentRotation += -180;
+    turnTurret(-180);
     
-    while(currentRotation < 180){
+    while(scanner_currentRotation < 180){
         // Turn turret a little to the right
-        RotateMotor(MOTOR_TURRET_OUT, MOTOR_TURRET_PWR, 
-            TURRET_ROTATION_AMOUNT);
-        currentRotation += TURRET_ROTATION_AMOUNT;
-        
+        turnTurret(TURRET_ROTATION_AMOUNT);
+
         // Wait for turret to rotate and the sensor to get a value
         Wait(TURRET_ROTATION_DELAY);
         
@@ -37,18 +48,13 @@ void ultrasonic_scan() {
 
         // Check if the value is above the threshold
         if (sensorValue > ULTRASONIC_THRESHOLD) {
-            // Found a clear direction, turn and stop scanning
-            TurnRobot(currentRotation, 50);
-
-            // Test message
-            ClearScreen();
-            TextOut(0,0,"Found free direction! :)");
-            Wait(1000);
-            break;
+            // Return the found direction
+            return sensorValue;
         }
     }
 
     // No free direction was found
     // Turn the turret back to its original position
-    RotateMotor(MOTOR_TURRET_OUT, MOTOR_TURRET_PWR, -currentRotation);
+    centerTurret();
+    return 0;
 }
