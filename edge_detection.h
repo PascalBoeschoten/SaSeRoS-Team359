caf5e1ddf93cf9b35b33b167ae22eb0fe868e11f
Pascal Boeschoten <passiepascal@gmail.com>
2013-04-16 10:55:31 +0300

    Edge detection works

--- edge_detection.h
+++ edge_detection.h
@@ -1,14 +1,12 @@
-#EDGE_LIGHT_THRESHOLD 100
+#define EDGE_LIGHT_THRESHOLD 300
 
 // Returns true if an edge is detected, false if not.
 bool DetectEdge() {
-	while(true) {
-		if(Sensor(SENSOR_LIGHT_EDGE) >= EDGE_LIGHT_THRESHOLD) {
-			// The sensor is pressed so there is no edge.
-			return false;
-        } else {
-			// The sensor isn't pressed so there is an edge.
-        	return true;
-        }
-	}
+	if(SENSOR_LIGHT_EDGE >= EDGE_LIGHT_THRESHOLD) {
+		// The sensor is detecting a lot of light so there is no edge.
+		return false;
+    } else {
+		// The sensor is detecting not much light so there is an edge.
+    	return true;
+    }
 }
